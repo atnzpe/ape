e6dc79f5c758f23ed5d11be4eef2cd4e45a7b15a
Gleyson Atanazio <gleysonasilva@gmail.com>
2024-08-23 18:08:51 -0300

    Tela de destinos criada

--- frontend/main.py
+++ frontend/main.py
@@ -1,21 +1,19 @@
+# Importa a biblioteca Flet
 import flet as ft
-from screens.home_screen import HomeScreen
-from screens.destinos_screen import DestinosScreen
 
+# Importa a tela HomeScreen do módulo screens.home_screen
+from screens.home_screen import HomeScreen  
 
+# Define a função principal do aplicativo
 def main(page: ft.Page):
-    page.title = "Apé - Seu Caminho em Pernambuco"
+    # Define o título da página que será exibido na aba do navegador
+    page.title = "Apé - Seu Caminho em Pernambuco_Main"  
 
-    # Cria as instâncias das telas
-    home_screen = HomeScreen()
-    destinos_screen = DestinosScreen()
+    # Cria uma instância da tela HomeScreen
+    home_screen = HomeScreen() 
 
-    # Adiciona as telas à página
-    page.add(home_screen, destinos_screen)
-
-    # Define a rota inicial (tela que será exibida primeiro)
-    page.go(home_screen.route)
-
+    # Adiciona a instância da tela HomeScreen à página principal
+    page.add(home_screen) 
 
 # Inicia o aplicativo Flet, definindo 'main' como a função principal
 ft.app(target=main)
--- frontend/screens/destinos_screen.py
+++ frontend/screens/destinos_screen.py
@@ -1,13 +1,12 @@
 import flet as ft
 
+
 def DestinosScreen():
-    return ft.View(  # Alterado para ft.View
-        route="/destinos",  # Rota da DestinosScreen 
-        controls=[  # Lista de controles da tela
+    return ft.Column(
+        [
             ft.AppBar(title=ft.Text("Destinos - Apé")),
             ft.Text(
-                "Em breve, uma lista de lugares incríveis para você conhecer!", size=20
-            )
+                "Em breve, uma lista de lugares incríveis para você conhecer!", size=20)
         ],
-        #expand=True,
+        expand=True,
     )
